3ed82035210f2c956f1cd300b8cc284945b8667a
Eric Potash <eric@k2co3.net>
2019-01-25 23:20:49 -0600

    boom

--- Makefile
+++ Makefile
@@ -1,18 +1,21 @@
-STATIONS=724940-23234
+STATIONS=724940-23234 725300-94846 723530-13967 744860-94789
 
-$(shell mkdir -p www csv/$(STATIONS))
+$(shell mkdir -p www $(STATIONS:%=csv/%))
 
-all: makefiles/$(STATIONS).mk $(STATIONS)
+MAKEFILES=$(STATIONS:%=makefiles/%.mk)
+
+all: $(MAKEFILES) $(STATIONS)
 
 www/isd-inventory.csv:
 	curl ftp://ftp.ncdc.noaa.gov/pub/data/noaa/isd-inventory.csv > $@
 
-makefiles/$(STATIONS).mk: template.mk www/isd-inventory.csv station_years.sh
+# sequence of all day-of-year csv files                                                                                   
+MONTHDAYS = $(shell python monthdays.py)
+
+$(MAKEFILES): template.mk www/isd-inventory.csv station_years.sh
 	set -e; \
 	export STATION=`basename $@ .mk`; \
 	export YEARS=`./station_years.sh $$STATION | tr '\n' ' '`; \
 	    cat $< | envsubst > $@
 
-include makefiles/$(STATIONS).mk
-
-
+include $(MAKEFILES)
--- template.mk
+++ template.mk
@@ -1,6 +1,6 @@
 # STATION and YEARS will get substituted by the master Makefile
-STATION=${STATION}
-YEARS=${YEARS}
+STATION := ${STATION}
+YEARS := ${YEARS}
 
 # sequence of station-years, e.g. 724940-23234-1987
 STATION_YEARS=$(YEARS:%=$(STATION)-%)
@@ -8,19 +8,18 @@ STATION_YEARS=$(YEARS:%=$(STATION)-%)
 # sequence of all gz files
 GZS = $(STATION_YEARS:%=www/%.gz)
 
-# sequence of all day-of-year csv files
-MONTHDAYS = $(shell python monthdays.py)
+# output csv files, one for each month-day
 CSVS = $(MONTHDAYS:%=csv/$(STATION)/%.csv)
 
 $(GZS):
-	wget -nv -O $@ ftp://ftp.ncdc.noaa.gov/pub/data/noaa/$(@:www/$(STATION)-%.gz=%)/$(@:www/%=%)
+	wget -nv -O $@ ftp://ftp.ncdc.noaa.gov/pub/data/noaa/`basename $@ .gz | cut -d- -f3`/$(@:www/%=%)
 
 csv/$(STATION).csv: $(GZS) isd2csv.sh isd2csv.py
-	./isd2csv.sh $(STATION) | python isd2csv.py > $@
+	./isd2csv.sh `basename $@ .csv` | python isd2csv.py > $@
 
 $(CSVS): csv/$(STATION).csv isd2hourly.py
 	# grep for header and matching dates
-	grep '^\(date_hour\|[[:digit:]]\{4\}'$(@:csv/$(STATION)/%.csv=%)'\)' $< | python isd2hourly.py > $@
+	grep '^\(date_hour\|[[:digit:]]\{4\}'`basename $@ .csv`'\)' $< | python isd2hourly.py > $@
 
 # a phony target for this Makefile
 $(STATION): $(CSVS) $(GZS) csv/$(STATION).csv
