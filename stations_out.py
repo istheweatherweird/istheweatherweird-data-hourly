ddc9d8c9237f23fab0744154b78c0c8829f8c2f2
Eric Potash <eric@k2co3.net>
2019-02-06 13:33:09 -0600

    add timezones to station list

--- stations_out.py
+++ stations_out.py
@@ -1,9 +1,17 @@
 # merge custom stations_in.csv with isd-history.csv from noaa ftp
 import pandas as pd
 import sys
+import requests
 
 stations_in = pd.read_csv(sys.argv[1], dtype='str')
 isd_history = pd.read_csv(sys.argv[2], dtype='str')
 
-stations_in.merge(isd_history, on=['USAF','WBAN']).to_csv(sys.stdout, index=False)
+stations = stations_in.merge(isd_history, on=['USAF','WBAN'])
+
+STATION_URL = 'https://api.weather.gov/stations/'
+for i, call in stations.ICAO.iteritems():
+    response = requests.get(url=STATION_URL + call).json()
+    stations.loc[i,'TZ'] = response['properties']['timeZone']
+
+stations.to_csv(sys.stdout, index=False)
 
